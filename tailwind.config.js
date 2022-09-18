418d09c6684fff34a107ca20482074fd3bb925bf
Jfsalamanca3 <Jfsalamanca3@gmail.com>
2022-09-18 18:56:08 -0500

    primer commit, cargando archivos del proyecto de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+	content: [
+		'./src/**/*.html',
+		'./index.html',
+		'./src/**/*.js',
+],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        norway: "url('../img/norway.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+      },
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        principal: "#1e40af",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9",
+      }),
+      textColor: {
+        principal: "#1e40af",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    width: ["responsive", "hover", "focus"],
+    extend: {},
+  },
+  plugins: [],
+};
